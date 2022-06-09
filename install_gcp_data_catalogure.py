ad40954717f88b42baa106754558a74465de828d
Nilesh Lathiya <nileshlathiya@gmail.com>
2022-06-09 21:59:05 +0100

    publishing code for deployment packaging

--- install_gcp_data_catalogure.py
+++ install_gcp_data_catalogure.py
@@ -1,8 +1,12 @@
 from google.cloud import storage
 from google.cloud import bigquery
 from utils.utils import run_shell_cmd , read_json
+import os
+import shutil
 
-## getting config values
+######### ################## ################## #########
+######### ########### getting config values
+######### ################## ################## #########
 job_config = read_json("config/config.json")
 resource_location = job_config["resource_location"]
 project_id = job_config["project_id"]
@@ -18,7 +22,14 @@ tag_table_schema = read_json(job_config["tag_extract_table_schema"])
 bq_view_data_catalog_bigquery_info = job_config["bq_view_data_catalog_bigquery_info"]
 bq_view_data_catalog_bi_tag_extract = job_config["bq_view_data_catalog_bi_tag_extract"]
 
-### Creating GCS Buckets and Folders
+function_data_catalogs = job_config["function_data_catalogs"]
+function_data_extract = job_config["function_data_extract"]
+trigger_bucket = job_config["trigger_bucket"]
+service_account = job_config["service_account"]
+
+######### ################## ######### ######### #########
+######### ############ Creating GCS Buckets and Folders
+######### ################## ######### ######### #########
 gcs_client = storage.Client(project=project_id)
 
 bucket = gcs_client.bucket(template_landing_bucket)
@@ -53,7 +64,10 @@ blob = bucket.blob('catalog_extract/')
 blob.upload_from_string('', content_type='application/x-www-form-urlencoded;charset=UTF-8')
 print ('Bucket creation completed for data extract area for reporting')
 
-### Create BQ datasets and tables for reporting
+
+######### ################## ######### ######### #########
+######### ############ Create BQ datasets and tables for reporting
+######### ################## ######### ######### #########
 client = bigquery.Client(project=project_id)
 
 client.delete_dataset(f"{project_id}.{reporting_dataset}", delete_contents=True, not_found_ok=True)  # Make an API request.
@@ -90,41 +104,47 @@ job = client.query(query=sql,location=resource_location)  # API request.
 job.result()  # Waits for the query to finish.
 print('Created new view "{}.{}.{}".'.format(job.destination.project,job.destination.dataset_id,job.destination.table_id ))
 
-# sql = """
-# CREATE VIEW `acn-uki-ds-data-ai-project.temp_data_catalog_extract.data_catalog_bigquery_info`
-# AS select * except (datasetJoinkey, joinkey ) from
-# (SELECT catalog_name||schema_name datasetJoinkey,creation_time dataset_creation_time ,  last_modified_time dataset_last_modified_time 
-# FROM acn-uki-ds-data-ai-project.`region-europe-west2`.INFORMATION_SCHEMA.SCHEMATA) datasets
-# inner join 
-# (select project_id||dataset_id datasetJoinkey , project_id||dataset_id||table_id joinkey, project_id,dataset_id,        table_id, TIMESTAMP_MILLIS(creation_time) table_creation_time,  TIMESTAMP_MILLIS(last_modified_time) table_last_modified_time,row_count      table_row_count,        size_bytes table_size_bytes      
-# from acn-uki-ds-data-ai-project.`region-europe-west2`.INFORMATION_SCHEMA.TABLES) tables on datasets.datasetJoinkey = tables.datasetJoinkey
-# inner join 
-# (SELECT table_catalog||table_schema||table_name joinkey, column_name,is_nullable column_is_nullable,data_type column_data_type   
-# FROM acn-uki-ds-data-ai-project.`region-europe-west2`.INFORMATION_SCHEMA.COLUMNS
-# ) columns on tables.joinkey = columns.joinkey
-# """.format(    project_id, reporting_dataset, bq_view_data_catalog_bigquery_info, project_id, resource_location, project_id, reporting_dataset, project_id, reporting_dataset)
-# print('Running view create script "{}" ', sql)
-# job = client.query(query=sql,location=resource_location)  # API request.
-# job.result()  # Waits for the query to finish.
-# print('Created new view "{}.{}.{}".'.format(job.destination.project,job.destination.dataset_id,job.destination.table_id ))
 
-# sql = """
-# CREATE VIEW `{}.{}.{}`
-# AS SELECT distinct bq_info.project_id,bq_info.dataset_id dataset_name,bq_info.table_id table_name,bq_info.column_name,tags.template_id,
-# tags.template_location,tags.tag_field_id,tags.tag_field_value,tags.extract_timestamp FROM data_catalog_extract.data_catalog_bigquery_info bq_info
-# left join data_catalog_extract.data_catalog_tag_extract tags on  bq_info.project_id =tags.project_id
-# and tags.dataset_name = bq_info.dataset_id and tags.table_name = bq_info.table_id and tags.column_name = bq_info.column_name
-# union all 
-# SELECT project_id,	dataset_name,	table_name,column_name,template_id,template_location,tag_field_id,
-# tag_field_value,extract_timestamp FROM data_catalog_extract.data_catalog_tag_extract tags
-# where dataset_name in ('data_catalog_dev','data_catalog_extract')
-# """.format(    project_id, reporting_dataset, bq_view_data_catalog_bi_tag_extract)
-# print('Running view create script "{}" ', sql)
-# job = client.query(sql)  # API request.
-# job.result()  # Waits for the query to finish.
-# print('Created new view "{}.{}.{}".'.format(job.destination.project,job.destination.dataset_id,job.destination.table_id ))
-
-### Deploy Cloud Functions
+######### ################## ######### ######### #########
+######### ############ Deploy Cloud Functions
+######### ################## ######### ######### #########
 print ('cloud function deployment is started. It may take upto 4 minutes')
-run_shell_cmd ('sh -x ./deploy_cloudfunction.sh')
+try:
+    shutil.rmtree('./temp/')
+except FileNotFoundError:
+    print("temp folder not available to delete")
+    
+os.mkdir('./temp/')
+shutil.copy('./main.py', './temp')
+shutil.copy('./requirements.txt', './temp')
+shutil.copytree('./config', './temp/config' , dirs_exist_ok=True)
+shutil.copytree('./utils', './temp/utils' , dirs_exist_ok=True)
+
+## this is to deploy gcp function  for data catalog tagging
+command_to_run_function_for_tagging = 'gcloud functions deploy ' + f"{function_data_catalogs}" +  \
+' --entry-point create_template_and_tag ' \
+' --runtime python37 ' \
+' --region ' + f"{resource_location}" + \
+' --trigger-resource ' + f"{trigger_bucket}" +  \
+' --service-account ' + f"{service_account}" + \
+' --trigger-event google.storage.object.finalize ' \
+' --retry'
+print('command_to_run_function for tagging : ' + command_to_run_function_for_tagging)
+run_shell_cmd (command_to_run_function_for_tagging, './temp')
+
+## this is to deploy gcp function for data extract
+command_to_run_function_for_extract = 'gcloud functions deploy ' + f"{function_data_extract}"  +  \
+' --entry-point extract_datacatalog_data ' \
+' --runtime python37 ' \
+' --region ' + f"{resource_location}" + \
+' --trigger-http ' \
+' --service-account ' + f"{service_account}" + \
+' --allow-unauthenticated'
+print('command_to_run_function for data extract : ' + command_to_run_function_for_extract)
+run_shell_cmd (command_to_run_function_for_extract, './temp')
+
+try:
+    shutil.rmtree('./temp/')
+except FileNotFoundError:
+    print("Wrong file or file path")
 print ('cloud function deployment completed')
